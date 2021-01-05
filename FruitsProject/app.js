81d6a19164c6c5ab0a964c5170f2c30b9a02544a
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2021-01-05 07:30:57 +0530

    Add files via upload

---
+++ FruitsProject/app.js
@@ -0,0 +1,20 @@
+//npm init
+//npm install mongodb
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const url ="mongodb://localhost:27017";
+
+const dbname = "fruitsDB";
+
+const client = new MongoClient(url);
+
+client.connect(function(err){
+  assert.equal(null, err);
+  console.log("Connected To MongoDB Server")
+  const db = client.db(dbName);
+
+  client.close();
+});
